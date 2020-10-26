25119b98b3682a60ae47669bc9d36b10db7b0095
zzmaxus <64233266+zzmaxus@users.noreply.github.com>
2020-10-26 18:04:09 -0500

    Moving, Camera following Player, Jump Implemented

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,68 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    private float accelerationForce = 10;
+
+    [SerializeField]
+    private float maxSpeed = 2;
+
+    [SerializeField]
+    private float jumpForce = 5;
+
+
+    private new Rigidbody rigidbody;
+    private Vector2 input;
+    private new Collider collider;
+    private bool isJumping;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
+        collider = GetComponent<Collider>();
     }
+
+    private void FixedUpdate()
+    {
+        Vector3 InputDirection = new Vector3(input.x, 0, input.y);
+
+        Vector3 cameraFlattenedForward = Camera.main.transform.forward;
+        cameraFlattenedForward.y = 0;
+        var cameraRotation = Quaternion.LookRotation(cameraFlattenedForward);
+
+        Vector3 cameraRelativeInputDirection = cameraRotation * InputDirection;
+
+        if (rigidbody.velocity.magnitude < maxSpeed)
+        {
+            rigidbody.AddForce(cameraRelativeInputDirection * accelerationForce, ForceMode.Acceleration);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Jump();
+        }
+
+
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+
+       
+    }
+
+    
+    private void Jump()
+    { 
+       if(!isJumping)
+        {
+            rigidbody.AddForce(0, jumpForce, 0);
+        }
+
     }
 }
