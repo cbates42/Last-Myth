7c1f9c223c339d11b4670a1126ae44516c6c1d7e
zzmaxus <64233266+zzmaxus@users.noreply.github.com>
2020-10-28 20:50:25 -0500

    Enemies implemented, move toward player and give a psuedo-gameover gamestate

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField]
+    private Transform player;
+    [SerializeField]
+    private float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+     
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, player.position, speed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Application.Quit();
+            Debug.Log("Game Over");
+        }
     }
 }
